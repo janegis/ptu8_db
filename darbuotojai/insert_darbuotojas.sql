5864f66f81202f05274f7cca0775ad5f200a0dad
janegis <janegis22@gmail.com>
2023-01-17 18:12:56 +0200

    No Comit

--- darbuotojai/insert_darbuotojas.sql
+++ darbuotojai/insert_darbuotojas.sql
@@ -1,4 +1,4 @@
 -- SQLite
 INSERT INTO darbuotojai (vardas, pavarde, atlyginimas)
-VALUES ("Egle", "Smaguolyte", 3000);
+VALUES ("Tautvitas", "mazonis", 6000);
 SELECT * FROM darbuotojai;
--- darbuotojai/update_darbuotojai.sql
+++ darbuotojai/update_darbuotojai.sql
@@ -1,3 +1,3 @@
-UPDATE darbuotojai SET pavarde="Jūraitienė" WHERE id=3;
+UPDATE darbuotojai SET pavarde="Kastinaite" WHERE id=3;
 SELECT * FROM darbuotojai;
 -- CRUD = Create Read Update Delete
